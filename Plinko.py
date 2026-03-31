49ac3cff292a5004ab8f08ef2a04219e1a900af0
Sinelyuh <ambalangodagesi@wgc.school.nz>
2026-04-01 10:36:03 +1300

    6

--- Plinko.py
+++ Plinko.py
@@ -2,9 +2,6 @@
 import random
 
 #Initialising variables
-rows = int(input('Enter the amount of rows (8/12/16):'))
-while rows!=8 and rows!=12 and rows!=16:
-    rows = int(input('Enter the amount of rows (8/12/16):'))
 money = 500
 bet = int(input('How much do you want each bet to be (no more than 500):'))
 while bet>500:
@@ -12,13 +9,20 @@ while bet>500:
     
 
 # 8/12/16 multipliers
-slots=[]
-if rows==8:
-    slots=[13,3,1.3,0.7,0.4,0.7,1.3,3,13]
-elif rows==12:
-    slots=[33,11,4,2,1.1,0.6,0.3,0.6,1.1,2,4,11,33]
-else:
-    slots = [110,41,10,5,3,1.5,1,0.5,0.3,0.5,1,1.5,3,5,10,41,110]
+def slots(rows):
+    slots=[]
+    if rows==8:
+        return [13,3,1.3,0.7,0.4,0.7,1.3,3,13]
+    elif rows==12:
+        return [33,11,4,2,1.1,0.6,0.3,0.6,1.1,2,4,11,33]
+    else:
+        return [110,41,10,5,3,1.5,1,0.5,0.3,0.5,1,1.5,3,5,10,41,110]
+
+#Getting the rows
+row = int(input('Enter the amount of rows (8/12/16):'))
+while row not in [8, 12, 16]:
+    row = int(input('Enter the amount of rows (8/12/16):'))
+active_slots =slots(row)
 
 #Starting the game
 while True:
@@ -41,13 +45,13 @@ while True:
     position = 0
 
     #To loop through the rows
-    for row in range(rows):
+    for row_one in range(row):
 
         #Spacing for the pyramid
-        print(" " * (rows - row), end="")
+        print(" " * (row - row_one), end="")
 
             #To get the indexes
-        for i in range(row + 1):
+        for i in range(row_one + 1):
             if i == position:
                 print("✪ ", end="")
             else:
@@ -59,10 +63,10 @@ while True:
         position = position + move
 
         #To check the end position
-        if position > row:
-            position = row
+        if position > row_one:
+            position = row_one
 
-    multiplier = slots[position]
+    multiplier = active_slots[position]
 
 #Printing the winning and losses
     print("Landed in slot", position)
